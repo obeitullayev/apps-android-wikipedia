db46519d268fdb6fdd930aa2a7eaff0d547e04fd
obeitullaev <beyocman@gmail.com>
2026-02-08 11:32:38 +0300

    Added OnboardingScreen.kt with variables include page elements

---
+++ app/src/androidTest/java/org/wikipedia/lesson03/OnboardingScreen.kt
@@ -0,0 +1,41 @@
+package org.wikipedia.lesson03
+
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.widget.NestedScrollView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.tabs.TabLayout
+import org.wikipedia.R
+import org.wikipedia.views.AppTextView
+
+val skipButton = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_skip_button,
+    R.string.onboarding_skip
+)
+val continueButton = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_forward_button,
+    R.string.onboarding_continue
+)
+val pageIndicator = listOf(TabLayout::class.java, R.id.view_onboarding_page_indicator)
+val scrollView = listOf(NestedScrollView::class.java, R.id.scoreView)
+val imageCentered = listOf(AppCompatImageView::class.java, R.id.imageViewCentered)
+val primaryText =
+    listOf(AppTextView::class.java,
+        R.id.primaryTextView,
+        R.string.onboarding_welcome_title_v2)
+
+val secondaryText = listOf(
+    AppTextView::class.java,
+    R.id.secondaryTextView,
+    R.string.onboarding_multilingual_secondary_text
+)
+val labelOption = listOf(AppTextView::class.java, R.id.option_label)
+val languageList = listOf(RecyclerView::class.java, R.id.languagesList)
+val addLanguageButton = listOf(
+    MaterialButton::class.java,
+    R.id.addLanguageButton,
+    R.string.onboarding_multilingual_add_language_text
+)
+
