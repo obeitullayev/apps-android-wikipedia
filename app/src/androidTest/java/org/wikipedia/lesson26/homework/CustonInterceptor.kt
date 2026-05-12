767fb187629476417e691d13577259b2aef1b5ea
obeitullaev <beyocman@gmail.com>
2026-05-12 19:03:33 +0300

    Added Custom Interceptor test

--- app/src/androidTest/java/org/wikipedia/lesson26/homework/CustonInterceptor.kt
+++ app/src/androidTest/java/org/wikipedia/lesson26/homework/CustonInterceptor.kt
@@ -2,15 +2,39 @@ package org.wikipedia.lesson26.homework
 
 import android.util.Log
 import androidx.test.espresso.ViewInteraction
-import com.kaspersky.kaspresso.interceptors.behavior.ViewBehaviorInterceptor
+import com.kaspersky.kaspresso.interceptors.watcher.testcase.TestRunWatcherInterceptor
+import com.kaspersky.kaspresso.testcases.models.info.TestInfo
 
-class MyInterceptors : ViewBehaviorInterceptor {
-    override fun <T> intercept(
-        interaction: ViewInteraction,
-        action: () -> T
-    ):T {
-    Log.i("KASPRESSO", "Start message")
-        var res = action()
-    return res
+class MyInterceptors : TestRunWatcherInterceptor {
+    override fun onTestStarted(testInfo: TestInfo){
+        Log.i("KASPRESSO", "Старт теста")
+    }
+
+    override fun onBeforeSectionStarted(testInfo: TestInfo){
+        Log.i("KASPRESSO", "Старт блока перед тестом")
+    }
+
+    override fun onMainSectionStarted(testInfo: TestInfo){
+        Log.i("KASPRESSO", "Старт блока теста")
+    }
+
+    override fun onAfterSectionStarted(testInfo: TestInfo){
+        Log.i("KASPRESSO", "Старт блока после теста")
+    }
+
+    override fun onBeforeSectionFinishedSuccess(testInfo: TestInfo){
+        Log.i("KASPRESSO", "Финиш блока перед тестом")
+    }
+
+    override fun onMainSectionFinishedSuccess(testInfo: TestInfo){
+        Log.i("KASPRESSO", "Финиш блока теста")
+    }
+
+    override fun onAfterSectionFinishedSuccess(testInfo: TestInfo){
+        Log.i("KASPRESSO", "Финиш блока после теста")
+    }
+
+    override fun onTestFinished(testInfo: TestInfo, success: Boolean){
+        Log.i("KASPRESSO", "Финиш теста")
     }
 }
--- app/src/androidTest/java/org/wikipedia/lesson26/homework/InterceptorTest.kt
+++ app/src/androidTest/java/org/wikipedia/lesson26/homework/InterceptorTest.kt
@@ -2,8 +2,11 @@ package org.wikipedia.lesson26.homework
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.kaspersky.components.alluresupport.withForcedAllureSupport
+import com.kaspersky.kaspresso.interceptors.behavior.ViewBehaviorInterceptor
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import com.kaspersky.kaspresso.testcases.core.testcontext.BaseTestContext
+import io.qameta.allure.kotlin.Allure.step
 import org.junit.Rule
 import org.junit.Test
 import org.wikipedia.lesson18.homework.exploreScreen.ExploreScreenNamed
@@ -13,13 +16,36 @@ import org.wikipedia.lesson19.homework.action
 import org.wikipedia.lesson19.homework.verify
 import org.wikipedia.main.MainActivity
 
-class SimpleTestNamed : TestCase(Kaspresso.Builder.withForcedAllureSupport(false)) {
+class SimpleTestInterceptor : TestCase(
+    kaspressoBuilder = Kaspresso.Builder.advanced().apply{
+        testRunWatcherInterceptors.add(MyInterceptors())
+    }) {
+
     @get:Rule
     val testRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun goToExploreAndTopRead() {
-        run {
+    fun logStartFinish() {
+        val testName = "Lifecycle_Logging_Test"
+
+        before("Подготовка к тесту") {
+            // Лог начала подготовки
+            testLogger.i("LOG_ORDER: Начинаем блок BEFORE для $testName")
+
+            step("Предусловие: Сброс состояния приложения") {
+                // Здесь может быть сброс кэша или БД
+                testLogger.i("LOG_ORDER: Выполняется шаг подготовки внутри BEFORE")
+            }
+
+        }.after {
+            // Лог начала очистки
+            testLogger.i("LOG_ORDER: Начинаем блок AFTER для $testName")
+
+            step("Постусловие: Закрытие ресурсов") {
+                testLogger.i("LOG_ORDER: Выполняется шаг очистки внутри AFTER")
+            }
+
+        }.run {
             step("Проверяет что '${OnboardingScreenNamed.skipButton}' нажимается") {
                 action.click(OnboardingScreenNamed.skipButton)
             }
